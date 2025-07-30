a2a45c21805bfafd744661e469136b4842df8dbc
dag <rubtub2314@gmail.com>
2025-07-30 19:14:36 +0900

    기존 export 결과물을 삭제하고 생성하는 로직 추가

--- src/mysql_to_sqlite3/__main__.py
+++ src/mysql_to_sqlite3/__main__.py
@@ -1,9 +1,14 @@
+import os
 from . import MySQLtoSQLite
 
 class Main:
     def __init__(self):
+        self.path = "../../Assets/StreamingAssets/Reference_Data.db"
+
+        os.remove(self.path)
+
         converter = MySQLtoSQLite(
-            sqlite_file="../../Assets/StreamingAssets/Reference_Data.db",
+            sqlite_file=self.path,
             mysql_user="developer",
             mysql_password="idlerpg20250706!",
             mysql_database="reference_data",
